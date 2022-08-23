240d3e441ca7beb7841a09b9752d3140f89ef899
Samir Dave <samirdave1992@gmail.com>
2022-08-22 19:28:04 -0500

    embedded books data

---
+++ doc.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="recommender systems",
+    page_icon="👋",
+)
+
+st.write("# Welcome to my recommender system app! 👋")
+
+st.sidebar.success("Select a options below.")
+
+st.markdown(
+    """
+Recommender systems are basically algorithms that are aimed at suggesting contents(products, music, movies,videos etc)
+
+
+### Types of recommender systems:
+1. Content Based
+2. Collaborative filtering
+3. Hybrid recommender
+
+**Content Based**:
+It recommends contents based on similar attributes. Example: Watching more action movies will lead to more recommendations of action movies on yor streaming platfrom
+The movie recommendation is content based where we find similar keywords in the description/content details
+Data Shape:4k rows
+Logic determined by: cosine similarity and vector spaces
+
+**Collaborative filtering**:
+This method is based on data collected from different users. Example: If User A likes Avengers and Spiderman, and has given 5 star rating. User B who starts watching Avengers will be recommended spiderman because the likelyhood is very high that user B will like it
+The book recommendation is collaborative filtering approach which is based on users who completed the books and have provided good reviews. Any new user who picks a book will get recommendations on books that are highly rated and completed by other users
+Data Shape: 58K rows
+Logic determined by: Nearest neighbors on aggregated data of more than 50 ratings on a book
+"""
+)
---
+++ pages/books📈.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+st.page_config = st.set_page_config(
+    layout="wide"
+)
+
+st.header("Books Recommender:Collaborative filtering")
+
+model=pickle.load(open('artifacts/model.pkl','rb'))
+books_name=pickle.load(open('artifacts/book_names.pkl','rb'))
+final_rating=pickle.load(open('artifacts/final_rating.pkl','rb'))
+book_pivot=pickle.load(open('artifacts/book_pivot.pkl','rb'))
+
+def fetch_poster(suggestion):
+    book_name=[]
+    ids_index=[]
+    poster_url=[]
+
+    for book_id in suggestion:
+        book_name.append(book_pivot.index[book_id])
+    
+    for name in book_name[0]:
+        ids=np.where(final_rating['title']==name)[0][0]
+        ids_index.append(ids)
+
+    for idx in ids_index:
+        url=final_rating.iloc[idx]['image_url']
+        poster_url.append(url)
+
+        #poster_url.append(url)
+
+
+
+def recommend_books(book_name):
+    book_list=[]
+    book_id=np.where(book_pivot.index==book_name)[0][0] #[0][0] will filter the exact location
+    distance,suggestion=model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1),n_neighbors=6)
+
+    poster_url=fetch_poster(suggestion)
+
+    for i in range(len(suggestion)):
+        books=book_pivot.index[suggestion[i]]
+        for j in books:
+            book_list.append(j)
+
+    return book_list,poster_url
+
+
+    
+
+
+selected_books=st.selectbox('Select/type a book:',books_name)
+if st.button('Show Recommendations:'):
+    recommendation_books,poster_url=recommend_books(selected_books)
+
+    st.write("**1**:",recommendation_books[1],':blue_book:')
+
+    st.write("**2**:",recommendation_books[2],':orange_book:')
+
+    st.write("**3**:",recommendation_books[3],':green_book:')
+
+    st.write("**4**:",recommendation_books[4],':notebook:')
+
+    st.write("**5**:",recommendation_books[5],':blue_book:')
+    
